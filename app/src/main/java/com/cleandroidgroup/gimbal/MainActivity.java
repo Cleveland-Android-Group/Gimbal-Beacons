d908adff1330ba62a8b96bfdc5b77e67bb341fc0
Anthony Greer <anthony.joseph.greer@gmail.com>
2015-05-07 20:12:48 -0400

    working for now

--- app/src/main/java/com/cleandroidgroup/gimbal/MainActivity.java
+++ app/src/main/java/com/cleandroidgroup/gimbal/MainActivity.java
@@ -3,6 +3,9 @@ package com.cleandroidgroup.gimbal;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gimbal.android.BeaconEventListener;
@@ -10,25 +13,53 @@ import com.gimbal.android.BeaconManager;
 import com.gimbal.android.BeaconSighting;
 import com.gimbal.android.Gimbal;
 
+import org.w3c.dom.Text;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+
 public class MainActivity extends ActionBarActivity {
 
     private BeaconEventListener beaconSightingListener;
     private BeaconManager beaconManager;
 
     private Toast theToast;
+    private ViewGroup rootView;
+
+    private HashMap<String, TextView> beaconMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rootView = (ViewGroup) findViewById(R.id.root_view);
+
+        beaconMap = new HashMap<String, TextView>();
+
         Gimbal.setApiKey(this.getApplication(), "97e35120-0c08-4318-b755-a88326fdc2d9");
         beaconSightingListener = new BeaconEventListener() {
             @Override
             public void onBeaconSighting(BeaconSighting sighting) {
-                Log.i("INFO", sighting.toString());
-                theToast.setText(sighting.toString());
-                theToast.show();
+
+                TextView tv;
+                String key = sighting.getBeacon().getIdentifier();
+                if(!beaconMap.containsKey(key))
+                {
+                    tv = new TextView(MainActivity.this);
+                    tv.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+                    beaconMap.put(key, tv);
+                    rootView.addView(tv);
+                } else
+                {
+                    tv = beaconMap.get(key);
+                }
+
+                tv.setText(String.format("Name: %s; Temp: %s; Battery: %s",
+                    sighting.getBeacon().getName(),
+                    sighting.getBeacon().getTemperature(),
+                    sighting.getBeacon().getBatteryLevel()));
             }
         };
         beaconManager = new BeaconManager();
